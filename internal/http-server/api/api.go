b6ceca67d2aa85b66a03d9871485de5f406344ff
egorh <egor.hoot@gmail.com>
2025-12-22 16:25:56 +0100

    Nest `/order` route under `/webhook` in API v1 router.

--- internal/http-server/api/api.go
+++ internal/http-server/api/api.go
@@ -46,8 +46,10 @@ func New(conf *config.Config, log *slog.Logger, handler Handler) error {
 	router.MethodNotAllowed(errors.NotAllowed(log))
 
 	router.Route("/api/v1", func(v1 chi.Router) {
-		v1.Route("/order", func(r chi.Router) {
-			r.Post("/", order.UpdateOrder(log, handler))
+		v1.Route("/webhook", func(r chi.Router) {
+			v1.Route("/order", func(r chi.Router) {
+				r.Post("/", order.UpdateOrder(log, handler))
+			})
 		})
 	})
 
