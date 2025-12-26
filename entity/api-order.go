7806f872f3c4ce0eef934c0c096b6a9ff4c5b71a
egorh <egor.hoot@gmail.com>
2025-12-26 13:32:20 +0100

    Fix validation tag syntax in `ApiOrder` and `ApiOrderedItem` structs

--- entity/api-order.go
+++ entity/api-order.go
@@ -1,15 +1,15 @@
 package entity
 
 type ApiOrder struct {
-	ZohoID       string           `json:"zoho_id" validation:"required"`
-	Status       string           `json:"status" validation:"required"`
-	GrandTotal   float64          `json:"grand_total" validation:"required"`
-	OrderedItems []ApiOrderedItem `json:"ordered_items" validation:"required,dive"`
+	ZohoID       string           `json:"zoho_id" validate:"required"`
+	Status       string           `json:"status" validate:"required"`
+	GrandTotal   float64          `json:"grand_total" validate:"required"`
+	OrderedItems []ApiOrderedItem `json:"ordered_items" validate:"required,dive"`
 }
 
 type ApiOrderedItem struct {
-	ZohoID   string  `json:"zoho_id" validation:"required"`
-	Price    float64 `json:"price" validation:"required,min=0.01"`
-	Total    float64 `json:"total" validation:"required,min=0.01"`
-	Quantity int     `json:"quantity" validation:"required,min=1"`
+	ZohoID   string  `json:"zoho_id" validate:"required"`
+	Price    float64 `json:"price" validate:"required,min=0.01"`
+	Total    float64 `json:"total" validate:"required,min=0.01"`
+	Quantity int     `json:"quantity" validate:"required,min=1"`
 }
